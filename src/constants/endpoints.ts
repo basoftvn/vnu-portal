c906785f228b9c49e2322d923d6cc1344847f2ff
beast462 <binhmattroi@gmail.com>
2022-08-21 05:07:24 +0700

    (constants) add endpoints

---
+++ src/constants/endpoints.ts
@@ -0,0 +1,7 @@
+export const Endpoints = {
+  Origin: 'http://dangkyhoc.vnu.edu.vn',
+  Login: 'http://dangkyhoc.vnu.edu.vn/dang-nhap',
+  ListByS: 'http://dangkyhoc.vnu.edu.vn/danh-sach-mon-hoc/1/1',
+  ListByA: 'http://dangkyhoc.vnu.edu.vn/danh-sach-mon-hoc/1/2',
+  ListByR: 'http://dangkyhoc.vnu.edu.vn/danh-sach-mon-hoc-da-dang-ky/1',
+};
