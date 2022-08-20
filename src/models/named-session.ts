c5487a79f5c32f518cffb2ee96e97537398e10c2
beast462 <binhmattroi@gmail.com>
2022-08-21 05:07:04 +0700

    (models) add named session

---
+++ src/models/named-session.ts
@@ -0,0 +1,42 @@
+import { CookieJar, jar } from 'request';
+import { Endpoints } from 'src/constants/endpoints';
+
+export class NamedSession {
+  private name: string;
+  private readonly cookieJar: CookieJar;
+
+  public constructor() {
+    this.cookieJar = jar();
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getCookieJar(): CookieJar {
+    return this.cookieJar;
+  }
+
+  public setCookieJar(cookieJar: CookieJar): void {
+    const newCookies = cookieJar.getCookies(Endpoints.Origin);
+    const oldCookies = this.cookieJar.getCookies(Endpoints.Origin);
+
+    for (const cookie of oldCookies)
+      this.cookieJar.setCookie(`${cookie.key}=`, Endpoints.Origin);
+
+    for (const cookie of newCookies)
+      this.cookieJar.setCookie(cookie, Endpoints.Origin);
+  }
+
+  public clone(): NamedSession {
+    const cloned = new NamedSession();
+
+    cloned.setCookieJar(this.cookieJar);
+
+    return cloned;
+  }
+}
