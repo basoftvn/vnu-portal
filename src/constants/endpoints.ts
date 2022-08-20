4d4508aa41f1e989dcd17b0958c6077b54c62eda
beast462 <binhmattroi@gmail.com>
2022-08-21 06:03:54 +0700

    (constants) update endpoints: add select and confirm registration endpoints

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -4,4 +4,6 @@ export const Endpoints = {
   ListByS: 'http://dangkyhoc.vnu.edu.vn/danh-sach-mon-hoc/1/1',
   ListByA: 'http://dangkyhoc.vnu.edu.vn/danh-sach-mon-hoc/1/2',
   ListByR: 'http://dangkyhoc.vnu.edu.vn/danh-sach-mon-hoc-da-dang-ky/1',
+  Select: 'http://dangkyhoc.vnu.edu.vn/chon-mon-hoc/$/1/1',
+  ConfirmRegistration: 'http://dangkyhoc.vnu.edu.vn/xac-nhan-dang-ky/1',
 };
