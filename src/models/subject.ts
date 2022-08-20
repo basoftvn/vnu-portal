2db52aa9906b698174b08f0d2d825336f4497241
beast462 <binhmattroi@gmail.com>
2022-08-21 05:07:13 +0700

    (models) add subject

---
+++ src/models/subject.ts
@@ -0,0 +1,11 @@
+export type Subject = {
+  id: number;
+  name: string;
+  creditCount: number;
+  code: string;
+  maxSlots: number;
+  currentSlots: number;
+  lecturer: string;
+  fee: number;
+  schedule: string;
+};
