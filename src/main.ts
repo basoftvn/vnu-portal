7f58acf377a7ac2847706f9f05b9187f48c578db
beast462 <binhmattroi@gmail.com>
2022-08-21 01:03:37 +0700

    (bootstrap) add command runner

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { CommandModule, CommandService } from 'nestjs-command';
+import parse from 'string-argv';
+
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
+import yargs = require('yargs');
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createApplicationContext(AppModule, {
+    logger: false,
+  });
+
+  try {
+    const service = await app.select(CommandModule).get(CommandService);
+    yargs.scriptName('');
+
+    process.stdin.on('data', (args) => {
+      service.exec(parse(args.toString()));
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await app.close();
+  }
 }
+
 bootstrap();
