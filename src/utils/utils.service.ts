202973499e31067f8126dff9f10ca801afc73a56
beast462 <binhmattroi@gmail.com>
2022-08-21 00:57:53 +0700

    (utils) update utils service: - add exit command - add description for clear command

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -1,4 +1,22 @@
+import { Command } from 'nestjs-command';
+
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UtilsService {}
+export class UtilsService {
+  @Command({
+    command: 'clear',
+    describe: 'Xoá màn hình',
+  })
+  public clear(): void {
+    console.clear();
+  }
+
+  @Command({
+    command: 'exit',
+    describe: 'Thoát',
+  })
+  public exit(): void {
+    process.exit(0);
+  }
+}
