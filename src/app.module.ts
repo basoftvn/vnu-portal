41e73a9def52b405a837c31e1d9cd25753552a9f
beast462 <binhmattroi@gmail.com>
2022-08-21 00:48:55 +0700

    (app) import utils module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { CommandModule } from 'nestjs-command';
 
 import { Module } from '@nestjs/common';
 
+import { UtilsModule } from './utils/utils.module';
+
 @Module({
-  imports: [CommandModule],
+  imports: [CommandModule, UtilsModule],
 })
 export class AppModule {}
