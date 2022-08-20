c071cd544882f9540a39ac1b8c3988fa01726216
beast462 <binhmattroi@gmail.com>
2022-08-21 01:39:09 +0700

    (app) import session module, remove login module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { CommandModule } from 'nestjs-command';
 import { Global, Module } from '@nestjs/common';
 
 import { AppService } from './app.service';
-import { LoginModule } from './login/login.module';
 import { UtilsModule } from './utils/utils.module';
+import { SessionModule } from './session/session.module';
 
 @Global()
 @Module({
-  imports: [CommandModule, LoginModule, UtilsModule],
+  imports: [CommandModule, UtilsModule, SessionModule],
   providers: [AppService],
   exports: [AppService],
 })
