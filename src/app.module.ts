b2966eea5908ebfc6530a5dd24b4ebde15048d94
beast462 <binhmattroi@gmail.com>
2022-08-22 11:02:05 +0700

    (app) update app module: remove schedule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,12 @@
 import { CommandModule } from 'nestjs-command';
 
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 
 import { CreditModule } from './credit/credit.module';
 import { SessionModule } from './session/session.module';
 import { UtilsModule } from './utils/utils.module';
 
 @Module({
-  imports: [
-    CommandModule,
-    CreditModule,
-    SessionModule,
-    UtilsModule,
-    ScheduleModule.forRoot(),
-  ],
+  imports: [CommandModule, CreditModule, SessionModule, UtilsModule],
 })
 export class AppModule {}
