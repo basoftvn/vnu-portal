a1ff81850c66c3a1c225b8b83bcf87739332af26
beast462 <binhmattroi@gmail.com>
2022-08-22 17:37:36 +0700

    (app) update app module: import io module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { CommandModule } from 'nestjs-command';
 import { Module } from '@nestjs/common';
 
 import { CreditModule } from './credit/credit.module';
+import { IoModule } from './io/io.module';
 import { SessionModule } from './session/session.module';
 import { UtilsModule } from './utils/utils.module';
 
 @Module({
-  imports: [CommandModule, CreditModule, SessionModule, UtilsModule],
+  imports: [CommandModule, CreditModule, SessionModule, UtilsModule, IoModule],
 })
 export class AppModule {}
